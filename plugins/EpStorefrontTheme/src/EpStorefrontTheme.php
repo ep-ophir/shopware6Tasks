5368d7faf73c361b6a59edc402090cdcb3d94b94
ep-ophir <NoHamsletKo@gmail.com>
2022-11-29 04:57:50 +0100

    main

---
+++ plugins/EpStorefrontTheme/src/EpStorefrontTheme.php
@@ -0,0 +1,10 @@
+<?php declare(strict_types=1);
+
+namespace EpStorefrontTheme;
+
+use Shopware\Core\Framework\Plugin;
+use Shopware\Storefront\Framework\ThemeInterface;
+
+class EpStorefrontTheme extends Plugin implements ThemeInterface
+{
+}
